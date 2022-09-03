eb73e7d672a6d1e48984e130fef1d3bae170763f
Bjarke Bruun <bb@humandevelopers.com>
2022-09-03 13:53:04 +0200

    Added updated main_test.go with dns.SetDNSServer() set to ns1.dns.services

--- main_test.go
+++ main_test.go
@@ -3,36 +3,36 @@ package main
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/jetstack/cert-manager/test/acme/dns"
-
-	"github.com/cert-manager/webhook-example/example"
 )
 
 var (
 	zone = os.Getenv("TEST_ZONE_NAME")
+	fqdn string
 )
 
 func TestRunsSuite(t *testing.T) {
-	// The manifest path should contain a file named config.json that is a
-	// snippet of valid configuration that should be included on the
-	// ChallengeRequest passed as part of the test cases.
-	//
+	fqdn = "_acme-challenge." + zone
 
-	// Uncomment the below fixture when implementing your custom DNS provider
-	//fixture := dns.NewFixture(&customDNSProviderSolver{},
-	//	dns.SetResolvedZone(zone),
-	//	dns.SetAllowAmbientCredentials(false),
-	//	dns.SetManifestPath("testdata/my-custom-solver"),
-	//	dns.SetBinariesPath("_test/kubebuilder/bin"),
-	//)
-	solver := example.New("59351")
-	fixture := dns.NewFixture(solver,
-		dns.SetResolvedZone("example.com."),
-		dns.SetManifestPath("testdata/my-custom-solver"),
-		dns.SetDNSServer("127.0.0.1:59351"),
-		dns.SetUseAuthoritative(false),
+	fixture := dns.NewFixture(&customDNSProviderSolver{},
+		dns.SetResolvedZone(zone),
+		dns.SetResolvedFQDN(fqdn),
+		dns.SetAllowAmbientCredentials(false),
+		dns.SetManifestPath("testdata/dns-services"),
+		dns.SetDNSServer("195.242.131.6:53"),
+		dns.SetDNSChallengeKey("dns-services-webhook-challenge-test"),
+		dns.SetPollInterval(2*time.Second),
+		//dns.SetBinariesPath("_test/kubebuilder/bin"),
 	)
+	//solver := example.New("59351")
+	//fixture := dns.NewFixture(solver,
+	//	dns.SetResolvedZone("example.com."),
+	//	dns.SetManifestPath("testdata/my-custom-solver"),
+	//	dns.SetDNSServer("127.0.0.1:59351"),
+	//	dns.SetUseAuthoritative(false),
+	//)
 	//need to uncomment and  RunConformance delete runBasic and runExtended once https://github.com/cert-manager/cert-manager/pull/4835 is merged
 	//fixture.RunConformance(t)
 	fixture.RunBasic(t)
