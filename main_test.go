e45c0bcaba75d165915f74220c074bbc9b680256
Bjarke Bruun <bb@humandevelopers.com>
2022-09-12 16:18:50 +0200

    Fixed main_test.go for using Secret in testing

--- main_test.go
+++ main_test.go
@@ -16,25 +16,16 @@ var (
 func TestRunsSuite(t *testing.T) {
 	fqdn = "_acme-challenge." + zone
 
-	fixture := dns.NewFixture(&customDNSProviderSolver{},
+	fixture := dns.NewFixture(&dnsServicesProviderSolver{},
 		dns.SetResolvedZone(zone),
 		dns.SetResolvedFQDN(fqdn),
 		dns.SetAllowAmbientCredentials(false),
 		dns.SetManifestPath("testdata/dns-services"),
 		dns.SetDNSServer("195.242.131.6:53"),
 		dns.SetDNSChallengeKey("dns-services-webhook-challenge-test"),
-		dns.SetPollInterval(2*time.Second),
-		//dns.SetBinariesPath("_test/kubebuilder/bin"),
+		dns.SetPollInterval(10*time.Second),
 	)
-	//solver := example.New("59351")
-	//fixture := dns.NewFixture(solver,
-	//	dns.SetResolvedZone("example.com."),
-	//	dns.SetManifestPath("testdata/my-custom-solver"),
-	//	dns.SetDNSServer("127.0.0.1:59351"),
-	//	dns.SetUseAuthoritative(false),
-	//)
-	//need to uncomment and  RunConformance delete runBasic and runExtended once https://github.com/cert-manager/cert-manager/pull/4835 is merged
-	//fixture.RunConformance(t)
+
 	fixture.RunBasic(t)
 	fixture.RunExtended(t)
 
